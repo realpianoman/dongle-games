b7b7adbc42d9d755aa9f31be9f41edc9ac7c5bc1
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-08-28 10:54:44 -0700

    Doubled cells

--- src/constants.h
+++ src/constants.h
@@ -5,7 +5,7 @@ inline constexpr uint32_t TARGET_FPS(12);
 inline constexpr uint32_t TARGET_FRAME_TIME(1000 / TARGET_FPS);
 
 // Rows and cols should both be divisible by CELL_SIZE
-inline constexpr int CELL_SIZE(4);
-inline constexpr int ROWS(20);
-inline constexpr int COLS(40);
+inline constexpr int CELL_SIZE(2);
+inline constexpr int ROWS(40);
+inline constexpr int COLS(80);
 } // namespace constants
